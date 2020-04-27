c2358e72ed29719179443a3cfc7871ae62b105f9
Borsuk <borsuk@macbook-pro-macbook.home>
2020-04-27 15:03:49 +0200

    Updates

---
+++ precache-manifest.658ee74e862c1c9bd7f777065a73dbaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38a2aa10f5dd7e5d14a79d4453b16c2",
+    "url": "/cv-react/index.html"
+  },
+  {
+    "revision": "a134ba9f739eb8a78adc",
+    "url": "/cv-react/static/css/main.cd9ef7d8.chunk.css"
+  },
+  {
+    "revision": "41f6d39e06a71c78bd3b",
+    "url": "/cv-react/static/js/2.4015c0f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cv-react/static/js/2.4015c0f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a134ba9f739eb8a78adc",
+    "url": "/cv-react/static/js/main.29fb7d2c.chunk.js"
+  },
+  {
+    "revision": "d23011c245e80a74ba64",
+    "url": "/cv-react/static/js/runtime-main.0025f9c8.js"
+  }
+]);
